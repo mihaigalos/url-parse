16c256d5914a906f210cea92c48f304b1b977094
Mihai Galos <mihaigalos@gmail.com>
2022-06-26 11:06:28 +0200

    Test port when no path

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod error;
+use regex::Regex;
 use std::collections::HashMap;
 
 use crate::error::ParseError;
@@ -53,6 +54,47 @@ impl Url {
             _ => return (None, split[0]),
         };
     }
+
+    fn mixout_port<'a>(input: &'a str) -> (Option<String>, &'a str, &'a str) {
+        let position_colon = input.find(":");
+        if position_colon.is_some() {
+            let before = &input[..position_colon.unwrap()];
+            let after = &input[position_colon.unwrap() + 1..];
+            let re = Regex::new(r"(\d+).*").unwrap();
+            let caps = re.captures(after).unwrap();
+            let port = if caps.len() > 1 {
+                Some(caps.get(1).unwrap().as_str().to_string())
+            } else if caps.len() == 1 {
+                let re = Regex::new(r"^(\d+)$").unwrap();
+                let caps = re.captures(after).unwrap();
+                if caps.len() == 1 {
+                    Some(caps.get(0).unwrap().as_str().to_string());
+                }
+                None
+            } else {
+                None
+            };
+            return (port, before, after);
+        } else {
+        }
+        (None, "", "")
+    }
+}
+
+#[test]
+fn test_mixout_port_works_when_typical() {
+    let input = "https://www.example.co.uk:443/blog/article/search?docid=720&hl=en#dayone";
+    let (_, rest) = Url::mixout_scheme(input);
+    let (port, _, _) = Url::mixout_port(rest);
+    assert_eq!(port.unwrap(), "443");
+}
+
+#[test]
+fn test_mixout_port_works_when_no_path() {
+    let input = "https://www.example.co.uk:443";
+    let (_, rest) = Url::mixout_scheme(input);
+    let (port, _, _) = Url::mixout_port(rest);
+    assert_eq!(port.unwrap(), "443");
 }
 
 #[test]
