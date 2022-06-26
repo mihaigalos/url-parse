a8d728ecccae400351fb0bfae5cb6596635be4c1
Mihai Galos <mihaigalos@gmail.com>
2022-06-26 18:52:02 +0200

    S3: Add to defaults

--- src/url/defaults.rs
+++ src/url/defaults.rs
@@ -2,10 +2,11 @@ use std::collections::HashMap;
 
 pub fn default_port_mappings() -> HashMap<&'static str, (u32, &'static str)> {
     let mut m = HashMap::new();
+    m.insert("ftp", (21, "File Transfer Protocol"));
     m.insert("http", (80, "Hypertext Transfer Protocol"));
     m.insert("https", (443, "Hypertext Transfer Protocol Secure"));
-    m.insert("ftp", (21, "File Transfer Protocol"));
     m.insert("ssh", (22, "SSH File Transfer Protocol"));
+    m.insert("s3", (443, "Amazon S3 File Transfer Protocol"));
     m
 }
 
