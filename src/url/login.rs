e306c8dc880d3509c234fb81e93edb56253182fc
Mihai Galos <mihaigalos@gmail.com>
2022-07-03 15:39:01 +0200

    Login: Working  user:pass parse in url

--- src/url/login.rs
+++ src/url/login.rs
@@ -10,14 +10,20 @@ impl Parser {
             None => input.len(),
         };
 
-        let re = Regex::new(r"(.*):(.*)@(.*):.*").unwrap();
+        let re = Regex::new(r"(.*)@(.*):.*").unwrap();
         let caps = re.captures(&input[..pos_path]);
 
         if caps.is_some() {
             let caps = caps.unwrap();
             return if caps.len() > 1 {
-                let user = caps.get(1).unwrap().as_str().to_string();
-                let pass = caps.get(2).unwrap().as_str().to_string();
+                let user_with_pass = caps.get(1).unwrap().as_str().to_string();
+                let (user, pass) = match user_with_pass.find(":") {
+                    Some(v) => (
+                        user_with_pass[..v].to_string(),
+                        user_with_pass[v + 1..].to_string(),
+                    ),
+                    None => (user_with_pass, "".to_string()),
+                };
                 Some((user, pass))
             } else {
                 None
@@ -41,6 +47,14 @@ fn test_mixout_login_works_when_full_url_with_login() {
     let input =
         "https://user:pass@www.example.co.uk:443/blog/article/search?docid=720&hl=en#dayone";
     let expected = ("user".to_string(), "pass".to_string());
+    let result = Parser::new(None).mixout_login(input).unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_mixout_login_works_when_user_only() {
+    let input = "https://user@www.example.co.uk:443/blog/article/search?docid=720&hl=en#dayone";
+    let expected = ("user".to_string(), "".to_string());
     let result = Parser::new(None).mixout_login(input).unwrap();
     assert_eq!(result, expected);
 }
