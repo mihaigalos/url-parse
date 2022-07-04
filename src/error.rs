2785f35a2848695fcdb40166b446dc8a6983bbfa
Mihai Galos <mihaigalos@gmail.com>
2022-07-04 21:24:38 +0200

    Error: Remove superfluous

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,3 @@ pub enum ParseError {
     UnknownError,
     Ok,
 }
-
-// impl From<ParseError> for ParseError {
-//     fn from(cause: ParseError) -> std::io::Error {
-//         std::io::Error::new(std::io::ErrorKind::Other, cause.to_string())
-//     }
-// }
