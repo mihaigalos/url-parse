a7f9cb56e80375ad9d3d4e53da3935aae154b554
Mihai Galos <mihaigalos@gmail.com>
2022-07-06 19:03:41 +0200

    Docs: Improve wording

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A library for parsing URLs.
+//! Other crates currently do not provide support for i.e. special schemes.
+//! That's because they aren't listed in the [whatwg](https://url.spec.whatwg.org/#url-miscellaneous) standard.
+//! `url-parse` provides some missing schemes (`sftp`, `ssh`, `s3`) and enables the user to specify custom schemes before parsing.
+
 pub mod error;
 pub mod url;
 pub mod utils;
