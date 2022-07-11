f303df53535f0ccc7e16c990fd81ff23e2ef3761
Mihai Galos <mihaigalos@gmail.com>
2022-07-11 18:49:02 +0200

    Domain: IPv4 parsing

--- src/url/domain.rs
+++ src/url/domain.rs
@@ -25,11 +25,41 @@ impl Parser {
             Some(pos) => &input[..pos],
             None => input,
         };
+
         let re = Regex::new(r"(.*?)\.(.*)").unwrap();
         let caps = re.captures(input).unwrap();
         return DomainFields {
             subdomain: Some(caps.get(1).unwrap().as_str()),
             domain: Some(caps.get(2).unwrap().as_str()),
+        };
+    }
+
+    /// Extract the domain fields from a ipv4 url.
+    ///
+    /// # Example
+    /// ```rust
+    /// use url_parse::url::Parser;
+    /// use url_parse::url::global::DomainFields;
+    /// let input = "https://1.2.3.4:443/blog/article/search?docid=720&hl=en#dayone";
+    /// let expected = DomainFields {
+    ///     subdomain: None,
+    ///     domain: Some("1.2.3.4"),
+    /// };
+    /// let result = Parser::new(None).mixout_domain_ipv4(input);
+    /// assert_eq!(result, expected);
+    /// ```
+    pub fn mixout_domain_ipv4<'a>(&self, input: &'a str) -> DomainFields<'a> {
+        let input = Utils::substring_after_login(self, input);
+        let input = Utils::substring_before_port(self, input);
+        let input = match input.find("/") {
+            Some(pos) => &input[..pos],
+            None => input,
+        };
+        let re = Regex::new(r"([0-9]+?)\.([0-9]+?)\.([0-9]+?)\.([0-9]+?)").unwrap();
+        let caps = re.captures(input).unwrap();
+        return DomainFields {
+            subdomain: None,
+            domain: Some(caps.get(0).unwrap().as_str()),
         };
     }
 }
@@ -38,6 +68,16 @@ impl Parser {
 mod tests {
     use super::*;
 
+    #[test]
+    fn test_mixout_domain_ipv4_when_typical() {
+        let input = "https://1.2.3.4:443/blog/article/search?docid=720&hl=en#dayone";
+        let expected = DomainFields {
+            subdomain: None,
+            domain: Some("1.2.3.4"),
+        };
+        let result = Parser::new(None).mixout_domain_ipv4(input);
+        assert_eq!(result, expected);
+    }
     #[test]
     fn test_mixout_domain_fields_works_when_typical() {
         let input = "https://www.example.com:443/blog/article/search?docid=720&hl=en#dayone";
