8ed89b515642af3367f5774e15550d829875078d
Mihai Galos <mihaigalos@gmail.com>
2022-07-17 07:38:35 +0200

    Readme: Improve wording

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 //! A library for parsing URLs.
 //!
-//! Other crates currently do not provide support for i.e. special schemes.
-//!
-//! That's because they aren't listed in the [whatwg](https://url.spec.whatwg.org/#url-miscellaneous) standard.
+//!No current other crate with support for i.e. special schemes. The reasoning (i.e. in [rust-url](https://github.com/servo/rust-url/pull/776#issuecomment-1159352270)) is that schemes need to be part of the [whatwg](https://url.spec.whatwg.org/#url-miscellaneous) standard to be supported.
 //!
 //! `url-parse` provides some missing schemes (`sftp`, `ssh`, `s3`) and enables the user to specify custom schemes before parsing.
 
