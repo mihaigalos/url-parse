7852307e8ca73e6d09dd255a562ef9f12877e489
Mihai Galos <mihaigalos@gmail.com>
2022-08-07 21:39:23 +0200

    Canonic path: Initial commit

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 use crate::core::Parser;
-
+use std::collections::HashMap;
 pub struct Utils;
 
 impl Utils {
@@ -123,6 +123,58 @@ impl Utils {
             None => input,
         }
     }
+
+    /// Partially matches a subpath in a path. Useful for i.e. GitHub absolute paths from URL hrefs.
+    /// # Example
+    /// ```rust
+    /// use url_parse::utils::Utils;
+    /// use url_parse::core::Parser;
+    /// let input = "https://github.com/mihaigalos/aim/releases/tag/1.5.4";
+    /// let subpath = "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+    /// let expected = "https://github.com/mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+    /// let result = Utils::canonicalize(&Parser::new(None), input, subpath);
+    /// assert_eq!(result, expected);
+    pub fn canonicalize<'a>(parser: &Parser, input: &'a str, subpath: &'a str) -> String {
+        let mut result = parser
+            .scheme(input)
+            .and_then(|s| Some(s.to_string() + "://"))
+            .or_else(|| Some("".to_string()))
+            .unwrap();
+
+        let (similarity, input_splits) = Utils::compute_similarity(parser, input, subpath);
+        let key_with_max_value = similarity.iter().max_by_key(|entry| entry.1).unwrap().0;
+
+        result += &input_splits[0..*key_with_max_value].join("/");
+        result = result + "/" + subpath;
+
+        result
+    }
+
+    fn compute_similarity<'a>(
+        parser: &Parser,
+        input: &'a str,
+        subpath: &'a str,
+    ) -> (HashMap<usize, usize>, Vec<&'a str>) {
+        let input = Utils::substring_after_scheme(&parser, input);
+        let input_splits = input.split("/").collect::<Vec<&str>>();
+        let subpath_splits = subpath.split("/").collect::<Vec<&str>>();
+
+        let mut similarity: HashMap<usize, usize> = HashMap::new();
+        let mut pos_subpath = 0;
+        let mut pos_match = 0;
+        for (pos_input, input_split) in input_splits.iter().enumerate() {
+            if input_split == &subpath_splits[pos_subpath] {
+                if pos_subpath == 0 {
+                    pos_match = pos_input;
+                }
+                pos_subpath += 1;
+                *similarity.entry(pos_match).or_insert(0) += 1;
+            } else {
+                pos_subpath = 0;
+            }
+        }
+        (similarity, input_splits)
+    }
 }
 
 #[cfg(test)]
@@ -211,6 +263,81 @@ mod tests {
         let expected = "".to_string();
         let parser = Parser::new(None);
         let result = Utils::substring_after_port(&parser, input);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_compute_similarity_hashmap_works_when_typical() {
+        let input = "https://github.com/mihaigalos/aim/releases/tag/1.5.4";
+        let subpath =
+            "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+        let expected_pos_begin_match: usize = 1;
+        let expected_count_path_matches: usize = 3;
+
+        let parser = Parser::new(None);
+        let (hashmap, _) = Utils::compute_similarity(&parser, input, subpath);
+        assert_eq!(
+            hashmap[&expected_pos_begin_match],
+            expected_count_path_matches
+        );
+    }
+
+    #[test]
+    fn test_compute_similarity_input_splits_works_when_typical() {
+        let input = "https://github.com/mihaigalos/aim/releases/tag/1.5.4";
+        let subpath =
+            "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+        let expected_input_splits: Vec<&str> = vec![
+            "github.com",
+            "mihaigalos",
+            "aim",
+            "releases",
+            "tag",
+            "1.5.4",
+        ];
+
+        let parser = Parser::new(None);
+        let (_, input_splits) = Utils::compute_similarity(&parser, input, subpath);
+        assert_eq!(input_splits, expected_input_splits);
+    }
+
+    #[test]
+    fn test_compute_similarity_works_when_multiple_partial_matches() {
+        let input = "https://github.com/mihaigalos/aim/fake/path/mihaigalos/aim/releases/tag/1.5.4";
+        let subpath =
+            "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+        let expected_pos_begin_match: usize = 5;
+        let expected_count_path_matches: usize = 3;
+
+        let parser = Parser::new(None);
+        let (hashmap, _) = Utils::compute_similarity(&parser, input, subpath);
+        assert_eq!(
+            hashmap[&expected_pos_begin_match],
+            expected_count_path_matches
+        );
+    }
+
+    #[test]
+    fn test_canonicalize_works_when_typical() {
+        let input = "https://github.com/mihaigalos/aim/releases/tag/1.5.4";
+        let subpath =
+            "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+        let expected = "https://github.com/mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+
+        let parser = Parser::new(None);
+        let result = Utils::canonicalize(&parser, input, subpath);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_canonicalize_works_when_multiple_partial_matches() {
+        let input = "https://github.com/mihaigalos/aim/fake/path/mihaigalos/aim/releases/tag/1.5.4";
+        let subpath =
+            "mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+        let expected = "https://github.com/mihaigalos/aim/fake/path/mihaigalos/aim/releases/download/1.5.4/aim-1.5.4-x86_64-unknown-linux-gnu.tar.gz";
+
+        let parser = Parser::new(None);
+        let result = Utils::canonicalize(&parser, input, subpath);
         assert_eq!(result, expected);
     }
 }
