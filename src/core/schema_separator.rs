9e7c03d2f2fbbf075016ef1f187a52801bee85b8
Mihai Galos <mihaigalos@gmail.com>
2022-10-10 18:11:11 +0200

    Schema separator: Implement conversion from String

--- src/core/schema_separator.rs
+++ src/core/schema_separator.rs
@@ -15,12 +15,22 @@ impl From<SchemaSeparator> for usize {
     }
 }
 
+impl From<SchemaSeparator> for String {
+    fn from(v: SchemaSeparator) -> String {
+        match v {
+            SchemaSeparator::Colon => ":".to_string(),
+            SchemaSeparator::ColonSlashSlash => "://".to_string(),
+            _ => "".to_string(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_schema_separator_works_when_colon_typical() {
+    fn test_schema_separator_to_usize_works_when_colon_typical() {
         let expected = 1;
         let input = SchemaSeparator::Colon;
 
@@ -30,7 +40,7 @@ mod tests {
     }
 
     #[test]
-    fn test_schema_separator_works_when_colon_slash_slash_typical() {
+    fn test_schema_separator_to_usize_works_when_colon_slash_slash_typical() {
         let expected = 3;
         let input = SchemaSeparator::ColonSlashSlash;
 
@@ -40,11 +50,41 @@ mod tests {
     }
 
     #[test]
-    fn test_schema_separator_works_when_unknown() {
+    fn test_schema_separator_to_usize_works_when_unknown() {
         let expected = 0;
         let input = SchemaSeparator::Unknown;
 
         let actual: usize = input.into();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_schema_separator_to_string_works_when_colon_typical() {
+        let expected = ":".to_string();
+        let input = SchemaSeparator::Colon;
+
+        let actual: String = input.into();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_schema_separator_to_string_works_when_colon_slash_slash_typical() {
+        let expected = "://".to_string();
+        let input = SchemaSeparator::ColonSlashSlash;
+
+        let actual: String = input.into();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_schema_separator_to_string_works_when_unknown() {
+        let expected = "";
+        let input = SchemaSeparator::Unknown;
+
+        let actual: String = input.into();
 
         assert_eq!(actual, expected);
     }
