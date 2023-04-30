7659fbcf25ec37bc9d3f3e364caa3c45d1468c9b
Mihai Galos <mihaigalos@gmail.com>
2023-04-30 23:02:59 +0200

    Version: Bump to 1.0.6

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 
 A library for parsing URLs.
 
-No current other crate with support for i.e. special schemes. The reasoning (i.e. in [rust-url](https://github.com/servo/rust-url/pull/776#issuecomment-1159352270)) is that schemes need to be part of the [whatwg](https://url.spec.whatwg.org/#url-miscellaneous) standard to be supported.
-
  `url-parse` provides some missing schemes (`sftp`, `ssh`, `s3`) and enables the user to specify custom schemes before parsing.
  # Example
  ```rust,no_run
