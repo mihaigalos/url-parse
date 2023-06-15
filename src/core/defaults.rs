0f19dccd289245d79d5db62fe58da579466f1f34
Mihai Galos <mihai@galos.one>
2023-06-15 20:59:09 +0200

    Defaults: Simplify hashmap

--- src/core/defaults.rs
+++ src/core/defaults.rs
@@ -3,13 +3,13 @@ use std::collections::HashMap;
 /// Get the default port mappings for well-known ports.
 /// This is a convenience function to create a Parser object (via `Parser::new()`) and pass it defaults.
 pub fn default_port_mappings() -> HashMap<&'static str, (u32, &'static str)> {
-    let mut m = HashMap::new();
-    m.insert("ftp", (21, "File Transfer Protocol"));
-    m.insert("http", (80, "Hypertext Transfer Protocol"));
-    m.insert("https", (443, "Hypertext Transfer Protocol Secure"));
-    m.insert("ssh", (22, "SSH File Transfer Protocol"));
-    m.insert("scp", (22, "SSH File Transfer Protocol"));
-    m.insert("sftp", (22, "SSH File Transfer Protocol"));
-    m.insert("s3", (443, "Amazon S3 File Transfer Protocol"));
-    m
+    HashMap::from([
+        ("ftp", (21, "File Transfer Protocol")),
+        ("http", (80, "Hypertext Transfer Protocol")),
+        ("https", (443, "Hypertext Transfer Protocol Secure")),
+        ("ssh", (22, "SSH File Transfer Protocol")),
+        ("scp", (22, "SSH File Transfer Protocol")),
+        ("sftp", (22, "SSH File Transfer Protocol")),
+        ("s3", (443, "Amazon S3 File Transfer Protocol")),
+    ])
 }
