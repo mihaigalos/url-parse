55dfe65644ed5de9c25b2f71fb975030a6fafedc
Fredrik Simonsson <fredrik@nodle.com>
2023-07-14 16:29:35 +0900

    Implement std::error::Error for ParseError
    ParseError is never emitted, so the dead enum members are removed.
    By implementing std::error::Error for the error type the user
    is able to unwrap with a ? and return a Box<dyn std::error::Error>

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,13 @@
+use core::fmt::Display;
+
 #[derive(Debug)]
 pub enum ParseError {
-    UnknownError,
-    Ok,
 }
+ 
+impl Display for ParseError {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "ParseError")
+    }
+}
+
+impl std::error::Error for ParseError {}
